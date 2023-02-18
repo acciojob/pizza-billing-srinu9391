c70330a69f7a14ea3b8f098da92895bdf1a83d7c
srinivasu jakka <jakkasrinivasu2001@gmail.com>
2023-02-18 13:56:54 +0530

    attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,18 @@
-package com.driver;
-
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
+    Pizza regularPizza = new Pizza("regular", "veg");
+    regularPizza.addExtraCheese();
+    regularPizza.addToppings();
+    regularPizza.addToppings();
+    regularPizza.addPaperBag();
+    regularPizza.printBill();
 
-    Pizza p = new Pizza(true);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+    System.out.println();
+
+    DeluxePizza deluxePizza = new DeluxePizza("non-veg");
+    deluxePizza.addToppings();
+    deluxePizza.addToppings();
+    deluxePizza.addExtraCheese();
+    deluxePizza.printBill();
   }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,68 @@
-package com.driver;
-
 public class Pizza {
+    private String type;
+    private String category;
+    private boolean extraCheese;
+    private int extraToppings;
+    private boolean paperBag;
+    private int basePrice;
+    private final int VEG_PRICE = 300;
+    private final int NON_VEG_PRICE = 400;
+    private final int EXTRA_CHEESE_PRICE = 80;
+    private final int VEG_TOPPINGS_PRICE = 70;
+    private final int NON_VEG_TOPPINGS_PRICE = 120;
+    private final int PAPER_BAG_PRICE = 20;
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
-
-    public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+    public Pizza(String type, String category) {
+        this.type = type;
+        this.category = category;
+        this.extraCheese = false;
+        this.extraToppings = 0;
+        this.paperBag = false;
+        if (this.category.equals("veg")) {
+            this.basePrice = VEG_PRICE;
+        } else {
+            this.basePrice = NON_VEG_PRICE;
+        }
     }
 
-    public int getPrice(){
-        return this.price;
+    public void addExtraCheese() {
+        if (!extraCheese) {
+            extraCheese = true;
+            basePrice += EXTRA_CHEESE_PRICE;
+        }
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addToppings() {
+        if (this.category.equals("veg")) {
+            basePrice += VEG_TOPPINGS_PRICE;
+        } else {
+            basePrice += NON_VEG_TOPPINGS_PRICE;
+        }
+        extraToppings++;
     }
 
-    public void addExtraToppings(){
-        // your code goes here
+    public void addPaperBag() {
+        if (!paperBag) {
+            paperBag = true;
+            basePrice += PAPER_BAG_PRICE;
+        }
     }
 
-    public void addTakeaway(){
-        // your code goes here
-    }
-
-    public String getBill(){
-        // your code goes here
-        return this.bill;
+    public void printBill() {
+        System.out.println("Base Price Of The Pizza: " + basePrice);
+        if (extraCheese) {
+            System.out.println("Extra Cheese Added: " + EXTRA_CHEESE_PRICE);
+        }
+        if (extraToppings > 0) {
+            if (category.equals("veg")) {
+                System.out.println("Extra Toppings Added: " + (extraToppings * VEG_TOPPINGS_PRICE));
+            } else {
+                System.out.println("Extra Toppings Added: " + (extraToppings * NON_VEG_TOPPINGS_PRICE));
+            }
+        }
+        if (paperBag) {
+            System.out.println("Paperbag Added: " + PAPER_BAG_PRICE);
+        }
+        System.out.println("Total Price: " + basePrice);
     }
 }
